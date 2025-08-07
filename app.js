8399788891f0f6384bc74712c862e1afa813863f
Johnny Benitez <johnnybeniitez@gmail.com>
2025-08-07 16:46:36 -0300

    Se creó la función actualizarListaAmigos()

--- app.js
+++ app.js
@@ -8,38 +8,47 @@ const ul = document.getElementById("listaAmigos");
 function agregarAmigo() {
   let amigo = document.getElementById("amigo").value;
 
-  console.log("nombre: " + amigo);//verificar input
+  console.log("nombre: " + amigo); //verificar input
 
   if (validarEntrada(amigo)) {
-    if (amigos.includes(amigo)) {
-      alert(`${amigo} ya está en la lista.`);
-      limpiar();
-    } else {
-      limpiar();
-      let li = document.createElement("li");
-      li.textContent = mayusPrimeraLetra(amigo);
-      ul.appendChild(li);
-      amigos.push(li.textContent);
-    }
+    actualizarListaAmigos(amigo);
   }
 
-  console.log("Lista de amigos: " + amigos);//verificar lista de amigos
-};
+  //console.log("Lista de amigos: " + amigos); //verificar lista de amigos
+}
+
+function actualizarListaAmigos(amigo) {
+  if (amigos.includes(`<li>${mayusPrimeraLetra(amigo)}</li>`)) {
+    alert(`${amigo} ya está en la lista.`);
+    limpiar();
+  } else {
+    limpiar();
+    amigos.push(`<li>${mayusPrimeraLetra(amigo)}</li>`);
+
+    ul.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+      ul.innerHTML += amigos[i];
+    }
+
+    console.log("Lista actualizada: " + amigos); //verificar lista actualizada
+  }
+
+}
 
 function sortearAmigo() {}
 function limpiar() {
-    document.getElementById("amigo").value = "";
-};
+  document.getElementById("amigo").value = "";
+}
 
 function validarEntrada(amigo) {
   if (amigo === "") {
     alert("Por favor, inserte un nombre.");
     return false;
-  };
+  }
   return true;
-};
+}
 
 function mayusPrimeraLetra(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
-};
-
+}
